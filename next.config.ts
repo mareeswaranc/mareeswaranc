46117ba9e16c4f8270cd5f23489691ac667c5bc0
Mareeswaran C <143170175+mareeswaranc@users.noreply.github.com>
2026-07-20 15:31:20 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,12 @@
 import type { NextConfig } from "next";
 
+const basePath = process.env.NODE_ENV === 'production' ? '/mareeswaranc' : '';
+
 const nextConfig: NextConfig = {
   output: 'export',
   images: { unoptimized: true },
+  basePath,
+  assetPrefix: basePath ? `${basePath}/` : undefined,
 };
 
 export default nextConfig;
